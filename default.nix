eede8772180df3e6ed5e9594f2046c727983a9eb
Michael Zhang <hmperson1+github@gmail.com>
2020-10-28 14:13:49 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ with pkgs;
 
 stdenv.mkDerivation {
   name = "formal-crypto-0.0.1-git";
-  buildInputs = [coq coqPackages.mathcomp_1_9];
+  buildInputs = [coq coqPackages.mathcomp];
   src = ./.;
   enableParallelBuilding = true;
   installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
